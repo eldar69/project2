3c526bd1480133f6af314a2f75ce3f62e33117c0
somtoo <somtooedeh26@gmail.com>
2020-02-27 21:17:37 +0100

    create function moveForward

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ let slideInterval = 4000;
 function getFigures() {
 return document.getElementById('carousel').getElementsByTagName('figure');
 }
+
+function moveForward() {
+    var pointer;
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+            pointer = i;
+        }
+    }
